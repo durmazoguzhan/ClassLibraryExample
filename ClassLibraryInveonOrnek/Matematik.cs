d512f1ddf47fddbcfa8c898b2635d2ea8436ae1d
durmazoguzhan <durmazoguzhan@yahoo.com>
2023-10-26 21:35:54 +0300

    init

---
+++ ClassLibraryInveonOrnek/Matematik.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibraryInveonOrnek
+{
+    public static class Matematik
+    {
+        public static decimal Topla(decimal sayi1, decimal sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+    }
+}
